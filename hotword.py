fc30734a68917d30f6e65c9e956526019b37ceee
ndessai <ndessai@gmail.com>
2019-03-07 23:38:05 -0500

    fix runtime issues

--- hotword.py
+++ hotword.py
@@ -47,7 +47,6 @@ WARNING_NOT_REGISTERED = """
     This device is not registered. This means you will not be able to use
     Device Actions or see your device in Assistant Settings. In order to
     register this device follow instructions at:
-
     https://developers.google.com/assistant/sdk/guides/library/python/embed/register-device
 """
 def checkdist():
@@ -67,15 +66,12 @@ def setunsetpin(pin, delay):
 	time.sleep(delay)
 	GPIO.output(pin, GPIO.LOW)
 
-
 def process_event(event):
 
     
     """Pretty prints events.
-
     Prints all events that occur with two spaces between each new
     conversation and a single space between turns of a conversation.
-
     Args:
         event(event.Event): The current event to process.
     """
@@ -90,8 +86,6 @@ def process_event(event):
     if event.type == EventType.ON_DEVICE_ACTION:
         for command, params in event.actions:
             print('Do command', command, 'with params', str(params))
-            steps = 1
-
             if command == "com.example.commands.MoveCar":
                 if params['number'] != None:
                     steps = params['number']
@@ -115,7 +109,7 @@ def process_event(event):
                         print("right backward by "+ steps)
                     else:
                         print("backward by "+ steps)
-            
+
 def main():
 
     GPIO.setmode(GPIO.BCM)
@@ -136,33 +130,64 @@ def main():
 
     print('Distance to obstacle', distToObstacle)
     
-    device_config_file = os.path.join(
+    
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--device-model-id', '--device_model_id', type=str,
+                        metavar='DEVICE_MODEL_ID', required=False,
+                        help='the device model ID registered with Google')
+    parser.add_argument('--project-id', '--project_id', type=str,
+                        metavar='PROJECT_ID', required=False,
+                        help='the project ID used to register this device')
+    parser.add_argument('--nickname', type=str,
+                        metavar='NICKNAME', required=False,
+                        help='the nickname used to register this device')
+    parser.add_argument('--device-config', type=str,
+                        metavar='DEVICE_CONFIG_FILE',
+                        default=os.path.join(
                             os.path.expanduser('~/.config'),
                             'googlesamples-assistant',
                             'device_config_library.json'
-                        )
-    credentials_file = os.path.join(
+                        ),
+                        help='path to store and read device configuration')
+    parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
                             os.path.expanduser('~/.config'),
                             'google-oauthlib-tool',
                             'credentials.json'
-                        )
-    with open(credentials_file, 'r') as f:
+                        ),
+                        help='path to store and read OAuth2 credentials')
+    parser.add_argument('--query', type=str,
+                        metavar='QUERY',
+                        help='query to send as soon as the Assistant starts')
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s ' + Assistant.__version_str__())
+
+    args = parser.parse_args()
+    with open(args.credentials, 'r') as f:
         credentials = google.oauth2.credentials.Credentials(token=None,
                                                             **json.load(f))
 
-    device_model_id = "mypi-f33e7-product"
+    device_model_id = None
     last_device_id = None
     try:
-        with open(device_config_file) as f:
+        with open(args.device_config) as f:
             device_config = json.load(f)
             device_model_id = device_config['model_id']
             last_device_id = device_config.get('last_device_id', None)
     except FileNotFoundError:
         pass
 
+    if not args.device_model_id and not device_model_id:
+        raise Exception('Missing --device-model-id option')
+
     # Re-register if "device_model_id" is given by the user and it differs
     # from what we previously registered with.
-    should_register = ("mypi-f33e7-product" != device_model_id)
+    should_register = (
+        args.device_model_id and args.device_model_id != device_model_id)
+
+    device_model_id = args.device_model_id or device_model_id
 
     with Assistant(credentials, device_model_id) as assistant:
         events = assistant.start()
@@ -173,20 +198,22 @@ def main():
 
         # Re-register if "device_id" is different from the last "device_id":
         if should_register or (device_id != last_device_id):
-            register_device("mypi-f33e7", credentials,
-                                device_model_id, device_id, "")
-            print("device registered")
-            pathlib.Path(os.path.dirname(device_config_file)).mkdir(
+            if args.project_id:
+                register_device(args.project_id, credentials,
+                                device_model_id, device_id, args.nickname)
+                pathlib.Path(os.path.dirname(args.device_config)).mkdir(
                     exist_ok=True)
-            with open(device_config_file, 'w') as f:
-                json.dump({
-                    'last_device_id': device_id,
-                    'model_id': device_model_id,
-                }, f)
-            
+                with open(args.device_config, 'w') as f:
+                    json.dump({
+                        'last_device_id': device_id,
+                        'model_id': device_model_id,
+                    }, f)
+            else:
+                print(WARNING_NOT_REGISTERED)
+
         for event in events:
-            if event.type == EventType.ON_START_FINISHED:
-                assistant.send_text_query("")
+            if event.type == EventType.ON_START_FINISHED and args.query:
+                assistant.send_text_query(args.query)
         
             process_event(event)
 
