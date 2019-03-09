ea29f36e8e1ad6cfcbffe3f73e19696a5de01909
omndessai <omndessai@gmail.com>
2019-03-09 17:30:02 -0500

    hotword.py

--- hotword.py
+++ hotword.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from __future__ import print_function
 import argparse
 import json
 import os.path
 import pathlib2 as pathlib
-import RPi.GPIO as GPIO
 import google.oauth2.credentials
 import time
 
@@ -28,11 +12,6 @@ from google.assistant.library import Assistant
 from google.assistant.library.event import EventType
 from google.assistant.library.file_helpers import existing_file
 from google.assistant.library.device_helpers import register_device
-
-
-
-
-
 
 import faulthandler
 faulthandler.enable()
@@ -43,38 +22,9 @@ except NameError:
     FileNotFoundError = IOError
 
 
-WARNING_NOT_REGISTERED = """
-    This device is not registered. This means you will not be able to use
-    Device Actions or see your device in Assistant Settings. In order to
-    register this device follow instructions at:
-    https://developers.google.com/assistant/sdk/guides/library/python/embed/register-device
-"""
-def checkdist():
-	GPIO.output(26, GPIO.HIGH)
-	time.sleep(0.000015)
-	GPIO.output(26, GPIO.LOW)
-	while not GPIO.input(19):
-		pass
-	t1 = time.time()
-	while GPIO.input(19):
-		pass
-	t2 = time.time()
-	return (t2-t1)*340/2
-
-def setunsetpin(pin, delay):
-	GPIO.output(pin, GPIO.HIGH)
-	time.sleep(delay)
-	GPIO.output(pin, GPIO.LOW)
 
 def process_event(event):
 
-    
-    """Pretty prints events.
-    Prints all events that occur with two spaces between each new
-    conversation and a single space between turns of a conversation.
-    Args:
-        event(event.Event): The current event to process.
-    """
     if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
         print()
 
@@ -111,37 +61,12 @@ def process_event(event):
                         print("backward by "+ steps)
 
 def main():
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    GPIO.setup(23, GPIO.OUT)
-    GPIO.setup(25, GPIO.OUT)
-    GPIO.setup(12, GPIO.OUT)
-    GPIO.setup(26, GPIO.OUT)
-    GPIO.setup(19, GPIO.IN)
-
-    #now i am going to calculate the distance
-    distToObstacle = checkdist()
-
-    print('Distance to obstacle', distToObstacle)
-    #setunsetpin(18, .5)
-
-    distToObstacle = checkdist()
-
-    print('Distance to obstacle', distToObstacle)
-    
-    
+    project_device_model_id = "mypi-f33e7-product"
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--device-model-id', '--device_model_id', type=str,
-                        metavar='DEVICE_MODEL_ID', required=False,
-                        help='the device model ID registered with Google')
     parser.add_argument('--project-id', '--project_id', type=str,
                         metavar='PROJECT_ID', required=False,
                         help='the project ID used to register this device')
-    parser.add_argument('--nickname', type=str,
-                        metavar='NICKNAME', required=False,
-                        help='the nickname used to register this device')
     parser.add_argument('--device-config', type=str,
                         metavar='DEVICE_CONFIG_FILE',
                         default=os.path.join(
@@ -150,22 +75,14 @@ def main():
                             'device_config_library.json'
                         ),
                         help='path to store and read device configuration')
-    parser.add_argument('--credentials', type=existing_file,
-                        metavar='OAUTH2_CREDENTIALS_FILE',
-                        default=os.path.join(
+    
+    creds_file = default=os.path.join(
                             os.path.expanduser('~/.config'),
                             'google-oauthlib-tool',
                             'credentials.json'
-                        ),
-                        help='path to store and read OAuth2 credentials')
-    parser.add_argument('--query', type=str,
-                        metavar='QUERY',
-                        help='query to send as soon as the Assistant starts')
-    parser.add_argument('-v', '--version', action='version',
-                        version='%(prog)s ' + Assistant.__version_str__())
-
+                        )
     args = parser.parse_args()
-    with open(args.credentials, 'r') as f:
+    with open(creds_file, 'r') as f:
         credentials = google.oauth2.credentials.Credentials(token=None,
                                                             **json.load(f))
 
@@ -179,45 +96,24 @@ def main():
     except FileNotFoundError:
         pass
 
-    if not args.device_model_id and not device_model_id:
-        raise Exception('Missing --device-model-id option')
-
     # Re-register if "device_model_id" is given by the user and it differs
     # from what we previously registered with.
-    should_register = (
-        args.device_model_id and args.device_model_id != device_model_id)
+    should_register = (project_device_model_id != device_model_id)
 
-    device_model_id = args.device_model_id or device_model_id
+    device_model_id = project_device_model_id or device_model_id
 
-    with Assistant(credentials, device_model_id) as assistant:
+    with Assistant(credentials, project_device_model_id) as assistant:
         events = assistant.start()
 
         device_id = assistant.device_id
         print('device_model_id:', device_model_id)
         print('device_id:', device_id + '\n')
 
-        # Re-register if "device_id" is different from the last "device_id":
-        if should_register or (device_id != last_device_id):
-            if args.project_id:
-                register_device(args.project_id, credentials,
-                                device_model_id, device_id, args.nickname)
-                pathlib.Path(os.path.dirname(args.device_config)).mkdir(
-                    exist_ok=True)
-                with open(args.device_config, 'w') as f:
-                    json.dump({
-                        'last_device_id': device_id,
-                        'model_id': device_model_id,
-                    }, f)
-            else:
-                print(WARNING_NOT_REGISTERED)
-
         for event in events:
-            if event.type == EventType.ON_START_FINISHED and args.query:
-                assistant.send_text_query(args.query)
+            if event.type == EventType.ON_START_FINISHED:
+                assistant.send_text_query("what is your name")
         
             process_event(event)
-
-    GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
