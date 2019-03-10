c07c52b7028ae0ec519915b95e81c994fa860cd9
ndessai <ndessai@gmail.com>
2019-03-10 16:45:09 -0400

    build fix

--- commands.py
+++ commands.py
@@ -15,6 +15,21 @@ from google.assistant.library.device_helpers import register_device
 import faulthandler
 faulthandler.enable()
 
+
+class Direction(Enum):
+    FORWARD = 1
+    BACKWARD = 2
+    RIGHTFORWARD = 3
+    LEFTFORWARD = 4
+    RIGHTBACKWARD = 5
+    LEFTBACKWARD = 6
+
+class Command:
+    def __init__ (self, direction, steps = 0):
+        self.direction = direction
+        self.steps = steps
+
+
 try:
     FileNotFoundError
 except NameError:
--- sharedtypes.py
+++ sharedtypes.py
@@ -1,14 +1,1 @@
 from enum import Enum 
-
-class Direction(Enum):
-    FORWARD = 1
-    BACKWARD = 2
-    RIGHTFORWARD = 3
-    LEFTFORWARD = 4
-    RIGHTBACKWARD = 5
-    LEFTBACKWARD = 6
-
-class Command:
-    def __init__ (self, direction, steps = 0):
-        self.direction = direction
-        self.steps = steps
